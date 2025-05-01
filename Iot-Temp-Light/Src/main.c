897d19ca0a023a4f566851151fff17724f0f81a0
Siwas666 <seimon656@gmail.com>
2025-04-30 20:25:29 -0600

    Se incializan y prueban funciones

--- Iot-Temp-Light/Src/main.c
+++ Iot-Temp-Light/Src/main.c
@@ -19,11 +19,11 @@ int main(void)
 {
 	//Clock enable bus RCC USART and GPIO
 	RCC_APB1ENR |= (1U << 18);
-	RCC_APB1ENR |= (1U << 0);
 	RCC_APB2ENR |= (0x3U << 3);
 
 	system_init_72MHz();
 	init_system();
+	delay_init();
 	comunicate_process();
 
     // Inicializar la UART
@@ -34,16 +34,21 @@ int main(void)
 
     while(1) {
 
-            // Detectar flanco de subida (botón presionado)
+    	delay_us(480);
+    	recive_data_ok();
+    	delay_us(480);
+        recive_data_ok();
+
+            /* Detectar flanco de subida (botón presionado)
             if((GPIOx_IDR(GPIOB_BASE) & (1U << 1)) && !button_pressed) {
                 //Marca del boton presionado
             	button_pressed = 1;
 
-                delay_ms(50);
+                delay_us(480);
                 recive_data_ok();
-                delay_ms(50);
+                delay_us(480);
                 recive_data_ok();
-                delay_ms(50);
+                delay_us(480);
 
 
             }
@@ -52,7 +57,7 @@ int main(void)
             if(!(GPIOx_IDR(GPIOB_BASE) & (1U << 1))) {
             	//Reinicio de variable para marcar el boton como no presionado
                 button_pressed = 0;
-        }
+        }*/
 
     }
 }
