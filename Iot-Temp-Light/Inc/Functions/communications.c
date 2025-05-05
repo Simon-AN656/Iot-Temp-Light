9a11d0e40d5ae1bc4ad3810c835e623d45587307
Siwas666 <seimon656@gmail.com>
2025-05-05 00:49:47 -0600

    Se quita funcion IRQ de USART3

--- Iot-Temp-Light/Inc/Functions/communications.c
+++ Iot-Temp-Light/Inc/Functions/communications.c
@@ -3,8 +3,6 @@
 #include "Headers/delay_functions.h"
 #include <string.h>
 
-volatile char rx_buffer[64];   // Definición del buffer de recepción
-volatile uint8_t rx_index = 0; // Definición del índice de recepción
 
 //Funcion para activar UART3
 void uart_init (void){
@@ -40,48 +38,6 @@ void uart_init (void){
 	NVIC_ISER1 |= (1U << 7);
 
 }
-
-void USART3_IRQHandler(void)
-{
-    if (USARTx_SR(USART3_BASE) & (1U << 5)) // RXNE
-    {
-        char dato = USARTx_DR(USART3_BASE);
-
-        // Si el dato es terminador y no hay nada en el buffer, descartar
-        if ((dato == '\r' || dato == '\n') && (rx_index == 0))
-            {
-        // No hacer nada, ya que es un terminador extra
-           return;
-           }
-
-        if (dato == '\r' || dato == '\n') {
-            rx_buffer[rx_index] = '\0';
-
-            // Procesar comando directamente:
-            if (strcmp((char*)rx_buffer, "ON G") == 0) {
-                transmit_string("Recibido G\r\n");
-            } else if (strcmp((char*)rx_buffer, "ON R") == 0) {
-                transmit_string("Recibido R\r\n");
-            }else if (strcmp((char*)rx_buffer, "ON B") == 0) {
-                transmit_string("Recibido B\r\n");
-            }
-            else {
-                transmit_string("Comando desconocido\r\n");
-                transmit_string((char*)rx_buffer);
-
-            }
-
-            rx_index = 0;
-        } else {
-            if (rx_index < sizeof(rx_buffer) - 1) {
-                rx_buffer[rx_index++] = dato;
-            } else {
-                rx_index = 0;
-            }
-        }
-    }
-}
-
 
 //Función para enviar un string a través de UART
 void transmit_string(const char* str) {
