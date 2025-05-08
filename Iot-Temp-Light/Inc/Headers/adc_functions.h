48eeaf762d7a9b56a1baeb6f6daf96ea6643f0c3
Siwas666 <seimon656@gmail.com>
2025-05-08 15:08:23 -0600

    Se quitan lineas sin funcion

--- Iot-Temp-Light/Inc/Headers/adc_functions.h
+++ Iot-Temp-Light/Inc/Headers/adc_functions.h
@@ -2,19 +2,13 @@
 #define ADC_FUNCTIONS_H
 #include <stdint.h>
 
-
 void get_temp_vref(uint16_t* direct_temp);
 float get_celsius(void);
 void float_to_str(float val, char *buffer, int prec);
 void send_temp(void);
-
 void send_vdda(void);
 void send_vsense(void);
-//void send_dvref(void);
 void send_dtemp(void);
-//float get_vdda(void);
 float get_vsense(void);
-
-
 
 #endif
